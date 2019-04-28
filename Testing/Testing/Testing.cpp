7fcd568b6f7f4ef312c72d4e5927ae0a53caad36
Aleksander <sanya69876@gmail.com>
2019-04-28 22:44:21 +0300

    add testing file

---
+++ Testing/Testing/Testing.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+#include "StringLib.h"
+using namespace std;
+
+int main()
+{
+	StringLib str, str2;
+/*
+	// Testing of >> and << operators
+	cout << "overload: cin >> string\nEnter string\n";
+	cin >> str;
+	cout << "\noverload: cout << string\nYour string is: " << str << endl;
+	cout << "\noverload: string << cin\nEnter string\n";
+	str << cin;
+	cout << "\noverload: string >> cout\nYour string is: ";
+	str >> cout << endl;
+
+	// Testing of += and + and =
+	char *temp_str = new char[100];
+
+	cout << "\nEnter temp string to be used for\n\t1) overload +\n\t2) overload =\n\t3) overload +=\n";
+	cin.getline(temp_str, 99);
+	cout << "\nOverload +\n";
+	string plus = (str + temp_str);
+	cout << "result: " << plus << endl;
+	cout << "Your string still is: " << str << endl;
+
+	cout << "\nOverload =\n";
+	str = temp_str;
+	cout << "Your string is: " << str << endl;
+
+	cout << "\nOverload +=\n";
+	str += temp_str;
+	cout << "Your string is: " << str << endl;
+
+	// Testing comparison operations
+	cout << "Enter second string to be used for comparison operations\nEnter string\n";
+	cin >> str2;
+	cout << "strings are: " << str << "     " << str2 << endl;
+	bool is_equal = str == str2;
+	cout << "Overload ==\nis Equal?: " << is_equal << endl;
+
+	bool is_not_equal = str != str2;
+	cout << "Overload !=\nis not Equal?: " << is_not_equal << endl;
+
+	cout << "Enter 2 strings to test <, >, <=, >= operations\n";
+	cin >> str;
+	cin >> str2;
+
+	cout << "str1 < str2? : " << (str < str2) << endl;
+	cout << "str1 <= str2? : " << (str <= str2) << endl;
+	cout << "str1 > str2? : " << (str > str2) << endl;
+	cout << "str1 >= str2? : " << (str >= str2) << endl;
+	*/
+
+
+	// Test class metods
+	cout << "Enter 2 strings to test class metods\n";
+
+
+
+	system("pause");
+}
